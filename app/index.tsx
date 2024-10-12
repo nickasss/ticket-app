f040793ab80c8f18bd191cab57d3db44868078f8
IamDalemark <danielweg123@gmail.com>
2024-10-13 00:12:39 +0800

    [wieg][camera]camera added

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,56 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { SafeAreaView, StyleSheet, Text, View, Button } from 'react-native'
+import { useRouter,Link } from 'expo-router'
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+
 
 const Index = () => {
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+  const [data, setData] = useState("");
+  const [cameraContent, setCameraContent] = useState();
+  const router = useRouter();
+  if (!cameraPermission) {
+    console.log("loading");
+    // Camera permissions are still loading.
+    return <View />;
+  }
+  if (!cameraPermission.granted) {
+    // Camera permissions are not granted yet.
+    console.log("asking");
+    return (
+      <View>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestCameraPermission} title="grant permission" />
+      </View>
+    );
+  }
+  console.log("permission granted");
   return (
-    <SafeAreaView>
+    <SafeAreaView style= {styles.container}>
       <Text>Camera Screen</Text>
-      <Link href={"/form"}>
+    <CameraView style={styles.camera} facing={'back'} onBarcodeScanned={({data}) => {
+      setData(data);
+      if (data) { 
+        router.push("/form")
+      }
+    }}>
+    </CameraView>
+    <Link href={"/form"}>
         <Text>Hello world</Text>
       </Link>
-    </SafeAreaView>
+    </SafeAreaView> 
   )
 }
 
 export default Index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  camera: {
+    height: '60%',
+    width: '60%',
+  },
+})
